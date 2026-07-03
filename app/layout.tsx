deaac1244898cb5968eb52be79d2f3fa6f4ed0f6
Abdul Mueed <mueed9972@gmail.com>
2026-07-03 18:02:47 +0500

    changed theme setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
       <body className="custom-scrollbar">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
